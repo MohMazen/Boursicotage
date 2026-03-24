b537df4bbe31b81806b837dede6edd4ef090b66a
Mohand Mazni <20245145@etud.univ-evry.fr>
2026-03-24 02:08:43 +0100

    Revert "interface v1"
    This reverts commit 7b184a4dfea349da9d2f49d901e1b41fef0ebd28.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Lobby from './pages/Lobby.jsx';
-import Game from './pages/Game.jsx';
-import Classement from './pages/Classement.jsx';
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/classement" element={<Classement />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
